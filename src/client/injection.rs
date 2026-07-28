eea8f82a75daa1dd3b867d4a7a45b96b774ba845
RubyOnWails <wutatf@protonmail.com>
2026-07-27 23:20:41 -0400

    fix: point profiles.host at sessionserver.ely.by for skin/profile lookups

--- src/client/injection.rs
+++ src/client/injection.rs
@@ -284,7 +284,7 @@ impl GameLauncher {
             jvm_args.push("-Dminecraft.api.auth.host=https://authserver.ely.by".into());
             jvm_args.push("-Dminecraft.api.session.host=https://sessionserver.ely.by".into());
             jvm_args.push("-Dminecraft.api.services.host=https://api.ely.by".into());
-            jvm_args.push("-Dminecraft.api.profiles.host=https://api.ely.by".into());
+            jvm_args.push("-Dminecraft.api.profiles.host=https://sessionserver.ely.by".into());
         }
 
         jvm_args.extend_from_slice(opts.custom_jvm_args);
